35da048f990701bb21fb280477d93abf768b7021
eomdh <edh9692@gmail.com>
2022-06-28 18:13:35 +0900

    action, state 정리

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,73 +1,124 @@
 export const initialState = {
-  isLoggingIn: false,  // 로그인 시도중
-  isLoggedIn: false,
-  isLoggingOut: false, // 로그아웃 시도중
+  logInLoading: false,  // 로그인 시도중
+  logInDone: false,     
+  logInError: null,     
+  logOutLoading: false, // 로그아웃 시도중
+  logOutDone: false,    
+  logOutError: null, 
+  signUpLoading: false, // 회원가입 시도중
+  signUpDone: false,    
+  signupError: null, 
   me: null,
   signUpData: {},
   loginData: {},
 }
 
+// 오타 방지를 위해 액션을 변수로 지정
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+export const FALLOW_REQUEST = 'FALLOW_REQUEST';
+export const FALLOW_SUCCESS = 'FALLOW_SUCCESS';
+export const FALLOW_FAILURE = 'FALLOW_FAILURE';
+
+export const UNFALLOW_REQUEST = 'UNFALLOW_REQUEST';
+export const UNFALLOW_SUCCESS = 'UNFALLOW_SUCCESS';
+export const UNFALLOW_FAILURE = 'UNFALLOW_FAILURE';
+
+const dummyUser = (data) => ({
+  ...data,
+  nickname: 'eomdh',
+  id: 1,
+  Posts: [],
+  Followings: [],
+  Followers: [],
+});
+
 export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN_REQUEST',
+    type: LOG_IN_REQUEST,
     data,
   }
 };
 
 export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT_REQUEST',
+    type: LOG_OUT_REQUEST,
   }
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOG_IN_REQUEST': {
+    case LOG_IN_REQUEST:
       return  {
         ...state,
-        isLoggingIn: true,
+        logInLoading: true,
+        logInDone: false,
+        logInError: null,
       }
-    };
-    case 'LOG_IN_SUCCESS': {
+    case LOG_IN_SUCCESS:
       return  {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: true,
-        me: { ...action.data, nickname: 'eomdh' }
+        logInLoading: false,
+        logInDone: true,
+        me: dummyUser(action.data),
       }
-    };
-    case 'LOG_IN_FAILURE': {
+    case LOG_IN_FAILURE: 
       return  {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: false,
+        logInLoading: false,
+        logInError: action.error,
       }
-    };
-    case 'LOG_OUT_REQUEST': {
+    case LOG_OUT_REQUEST:
       return  {
         ...state,
-        isLoggingOut: true,
+        logOutLoading: true,
+        logOutDone: false,
+        logOutError: null,
       }
-    };
-    case 'LOG_OUT_SUCCESS': {
+    case LOG_OUT_SUCCESS:
       return  {
         ...state,
-        isLoggingOut: false,
-        isLoggedIn: false,
+        logOutLoading: false,
+        logOutDone: true,
         me: null,
       }
-    };
-    case 'LOG_OUT_FAILURE': {
+    case LOG_OUT_FAILURE: 
       return  {
         ...state,
-        isLoggingOut: false,
+        logOutLoading: false,
+        logOutError: action.error,
       }
-    };
-    default: {
-      return {
+    case SIGN_UP_REQUEST:
+      return  {
         ...state,
+        signUpLoading: true,
+        signUpDone: false,
+        signUpError: null,
       }
-    }      
+    case SIGN_UP_SUCCESS:
+      return  {
+        ...state,
+        signUpLoading: false,
+        signUpDone: true,
+      }
+    case SIGN_UP_FAILURE: 
+      return  {
+        ...state,
+        signUpLoading: false,
+        signUpError: action.error,
+      }
+    default:
+      return state;
   }
 };
 
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,5 +1,10 @@
 import { all, delay, fork, put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
+import { 
+  LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE, 
+  LOG_OUT_REQUEST, LOG_OUT_SUCCESS, LOG_OUT_FAILURE,
+  SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
+} from "../reducers/user";
 
 function logInAPI(data) {
   return axiosos.post('/api/login', data)
@@ -10,13 +15,13 @@ function* logIn(action) {
     // const result = yield call(logInAPI, action.data);
     yield delay(1000);  // 서버가 없기 떄문에 비동기적인 효과
     yield put({
-      type: 'LOG_IN_SUCCESS',
-      data: action.data
+      type: LOG_IN_SUCCESS,
+      data: action.data,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_IN_FAILURE',
-      data: err.response.data
+      type: LOG_IN_FAILURE,
+      error: err.response.data,
     })
   }
 }
@@ -30,27 +35,51 @@ function* logOut() {
     // const result = yield call(logOutAPI);
     yield delay(1000);
     yield put({
-      type: 'LOG_OUT_SUCCESS',
+      type: LOG_OUT_SUCCESS,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_OUT_FAILURE',
-      data: err.response.data
+      type: LOG_OUT_FAILURE,
+      error: err.response.data,
+    })
+  }
+}
+
+function signUpAPI() {
+  return axios.post('/api/signup')
+}
+
+function* logOut() {
+  try {
+    // const result = yield call(signUpAPI);
+    yield delay(1000);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data,
     })
   }
 }
 
 function* watchLogIn() {
-  yield takeLatest('LOG_IN_REQUEST', logIn);
+  yield takeLatest(LOG_IN_REQUEST, logIn);
 }
 
 function* watchLogOut() {
-  yield takeLatest('LOG_OUT_REQUEST', logOut);
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, logOut);
 }
 
 export default function* userSaga() {
   yield all([
     fork(watchLogIn),
     fork(watchLogOut),
+    fork(watchSignUp),
   ])
 }

0da2a6040f7a7b72fa7b8c5f60a65981d2752615
eomdh <edh9692@gmail.com>
2022-07-04 23:56:29 +0900

    로그인 구현

--- back/passport/index.js
+++ back/passport/index.js
@@ -1,13 +1,20 @@
 const passport = require('passport');
 const local = require('./local');
+const { User } = require('../models');
 
 module.exports = () => {
-  passport.serializeUser(() => {
-
+  passport.serializeUser((user, done) => {
+    done(null, user.id);  // user정보중에서 쿠키랑 묶어줄 id만    
   });
 
-  passport.deserializeUser(() => {
-
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id }})
+      done(null, user);
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
   });
 
   local();
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const { User } = require('../models'); // 구조분해할당
+const { User, Post } = require('../models'); // 구조분해할당
 
 const router = express.Router();
-
-router.post('/login', (req, res, next) => { // 미들웨어 확장
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {  // 서버에러
-      console.error(err);
-      return next(err);
-    }
-    if (info) { // 클라이언트에러
-      return res.status(401).send(info.reason);
-    }
-    return req.login(user, async (loginErr) => {
-      if(loginErr) {  // passport 에러
-        console.errer(loginErr);
-        return next(loginErr);
-      }
-      return res.json(user);
-    });
-  })(req, res, next);
-});
 
 router.post('/', async (req, res, next) => {  // POST /user/
   try {
@@ -54,6 +35,46 @@ router.post('/', async (req, res, next) => {  // POST /user/
     console.error(error);
     next(error);  // status 500
   }
+});
+
+router.post('/login', (req, res, next) => { // 미들웨어 확장
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {  // 서버에러
+      console.error(err);
+      return next(err);
+    }
+    if (info) { // 클라이언트에러
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if(loginErr) {  // passport 에러
+        console.errer(loginErr);
+        return next(loginErr);
+      }
+      const fullUserWithoutPassword = await User.findOne({
+        where:  { id: user.id },
+        attributes: {
+          exclude: ['password']   // 전체 데이터에서 password만 제외
+        },
+        include: [{
+          model: Post,
+        }, {
+          model: User,
+          as: 'Followings',
+        }, {
+          model: User,
+          as: 'Followers',
+        }]
+      })
+      return res.status(200).json(fullUserWithoutPassword);
+    });
+  })(req, res, next);
+});
+
+router.post('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('ok');
 });
 
 module.exports = router;

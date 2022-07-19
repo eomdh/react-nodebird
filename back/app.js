e0b1f90526e6daf0c64ce7eb44e7adf22cd3d308
eomdh <edh9692@gmail.com>
2022-07-19 13:40:02 +0900

    cookie sharing

--- back/app.js
+++ back/app.js
@@ -50,6 +50,11 @@ app.use(session({
   saveUninitialized: false,
   resave: false,
   secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    domain: process.env.NODE_ENV === 'production' && '.nodebird.online'
+  },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,1 @@
-export const backUrl = 'http://3.37.158.69';
+export const backUrl = 'http://api.nodebird.online';

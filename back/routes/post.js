06850676ef419f15885fdda31cda1b3af4421ad9
eomdh <edh9692@gmail.com>
2022-07-19 14:11:42 +0900

    multer-s3

--- back/routes/post.js
+++ back/routes/post.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+
 const { Post, User, Comment, Image, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
@@ -13,15 +16,17 @@ try {
   fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads');
-    },
-    filename(req, file, done) {   // (ex: image.png)             
-      const ext = path.extname(file.originalname);  // 확장자 추출 (.png)
-      const basename = path.basename(file.originalname, ext);   // (image)
-      done(null, basename + '_' + new Date().getTime() + ext); // image202207071212.png
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'react-nodebird-eomdh',
+    key(req, file, cb) {
+      cb(null, `original/${Date.new()}_${path.basename(file.originalname)}`)
     }
   }),
   limits: { fileSize: 20 * 1024 * 1024 }, // 20MB (파일 사이즈 제한)
@@ -92,7 +97,7 @@ router.delete('/:postId', isLoggedIn, async (req, res, next) => {    // DELETE /
 
 router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {  // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.filename));
+  res.json(req.files.map((v) => v.location));
 });
 
 router.post('/:postId/comment', isLoggedIn, async (req, res) => {

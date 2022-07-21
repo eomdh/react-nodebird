47bb4b01ae7db5f90abfc363055d5412921f7996
eomdh <edh9692@gmail.com>
2022-07-21 19:21:11 +0900

    Fix Profile Page

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,10 +1,10 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Card, Avatar, Button } from 'antd';
 import Link from 'next/link';
-import { Avatar, Button, Card } from 'antd';
 import styled from 'styled-components';
 
-import { LOG_OUT_REQUEST } from '../reducers/user';
+import { logoutRequestAction } from '../reducers/user';
 
 const ButtonWrapper = styled(Button)`
   margin-top: 20px;
@@ -14,31 +14,29 @@ const UserProfile = () => {
   const dispatch = useDispatch();
   const { me, logOutLoading } = useSelector((state) => state.user);
 
-  const onLogout = useCallback(() => {
-    dispatch({
-      type: LOG_OUT_REQUEST,
-    });
+  const onLogOut = useCallback(() => {
+    dispatch(logoutRequestAction());
   }, []);
-  
+
   return (
     <Card
       actions={[
         <div key="twit"><Link href={`/user/${me.id}`}><a>짹짹<br />{me.Posts.length}</a></Link></div>,
-        <div key="followings"><Link href={`profile`}><a>팔로잉<br />{me.Followings.length}</a></Link></div>,
-        <div key="followers"><Link href={`profile`}><a>팔로워<br />{me.Followers.length}</a></Link></div>
+        <div key="followings"><Link href="/profile"><a>팔로잉<br />{me.Followings.length}</a></Link></div>,
+        <div key="followings"><Link href="/profile"><a>팔로워<br />{me.Followers.length}</a></Link></div>,
       ]}
     >
       <Card.Meta
         avatar={(
-          <Link href={`/user/${me.id}`}>
-            <Avatar>{me.nickname[0]}</Avatar>
+          <Link href={`/user/${me.id}`} prefetch={false}>
+            <a><Avatar>{me.nickname[0]}</Avatar></a>
           </Link>
         )}
         title={me.nickname}
       />
-      <ButtonWrapper onClick={onLogout} loading={logOutLoading}>로그아웃</ButtonWrapper>
+      <ButtonWrapper onClick={onLogOut} loading={logOutLoading}>로그아웃</ButtonWrapper>
     </Card>
-  )
-}
+  );
+};
 
 export default UserProfile;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useCallback} from 'react';
-import Router from 'next/router';
+import React, { useEffect, useState, useCallback } from 'react';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
+import Router from 'next/router';
 import { END } from 'redux-saga';
 import axios from 'axios';
 import useSWR from 'swr';
@@ -11,30 +11,30 @@ import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 import wrapper from '../store/configureStore';
+import { backUrl } from '../config/config';
 
 const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
-
   const [followersLimit, setFollowersLimit] = useState(3);
   const [followingsLimit, setFollowingsLimit] = useState(3);
 
-  const { data: followersData, error: followerError } = useSWR(`http://localhost:3065/user/followers?limit=${followersLimit}`, fetcher);
-  const { data: followingsData, error: followingError } = useSWR(`http://localhost:3065/user/followings?limit=${followingsLimit}`, fetcher);
+  const { data: followersData, error: followerError } = useSWR(`${backUrl}/user/followers?limit=${followersLimit}`, fetcher);
+  const { data: followingsData, error: followingError } = useSWR(`${backUrl}/user/followings?limit=${followingsLimit}`, fetcher);
 
   useEffect(() => {
-    if(!(me && me.id)) {
+    if (!(me && me.id)) {
       Router.push('/');
     }
   }, [me && me.id]);
 
-  const loadMoreFollowers = useCallback(() => {
-    setFollowersLimit((prev) => prev + 3);
-  }, []);
-
   const loadMoreFollowings = useCallback(() => {
     setFollowingsLimit((prev) => prev + 3);
+  }, []);
+
+  const loadMoreFollowers = useCallback(() => {
+    setFollowersLimit((prev) => prev + 3);
   }, []);
 
   if (!me) {
@@ -43,34 +43,36 @@ const Profile = () => {
 
   if (followerError || followingError) {
     console.error(followerError || followingError);
-    return <div>팔로잉/팔로워 로딩 중 에러 발생</div>;
+    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>;
   }
 
   return (
-    <AppLayout>
+    <>
       <Head>
-        <title>NodeBird | 내 프로필</title>
+        <title>내 프로필 | NodeBird</title>
       </Head>
-      <NicknameEditForm />
-      <FollowList header="팔로잉" data={followingsData} onClickMore={loadMoreFollowings} loading={!followingsData && !followingError}/>
-      <FollowList header="팔로워" data={followersData} onClickMore={loadMoreFollowers} loading={!followersData && !followerError}/>
-    </AppLayout>
-  )
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉" data={followingsData} onClickMore={loadMoreFollowings} loading={!followingsData && !followingError} />
+        <FollowList header="팔로워" data={followersData} onClickMore={loadMoreFollowers} loading={!followersData && !followerError} />
+      </AppLayout>
+    </>
+  );
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  console.log('getServerSideProps Start');
+  console.log('getServerSideProps start');
   console.log(context.req.headers);
   const cookie = context.req ? context.req.headers.cookie : '';
   axios.defaults.headers.Cookie = '';
-  if(context.req && cookie) {
+  if (context.req && cookie) {
     axios.defaults.headers.Cookie = cookie;
   }
   context.store.dispatch({
     type: LOAD_MY_INFO_REQUEST,
-  })
+  });
   context.store.dispatch(END);
-  console.log('getServerSideProps End');
+  console.log('getServerSideProps end');
   await context.store.sagaTask.toPromise();
 });
 
